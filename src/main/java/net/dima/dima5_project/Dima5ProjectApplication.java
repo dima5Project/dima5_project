f210b06f121a1a7e6d963dc7318db6c1c03515ba
Hyejin Kim <beckey7124@gmail,com>
2025-07-29 16:35:55 +0900

    전체 파일 구조

---
+++ src/main/java/net/dima/dima5_project/Dima5ProjectApplication.java
@@ -0,0 +1,13 @@
+package net.dima.dima5_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dima5ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dima5ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dima/dima5_project/Dima5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dima.dima5_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dima5ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
