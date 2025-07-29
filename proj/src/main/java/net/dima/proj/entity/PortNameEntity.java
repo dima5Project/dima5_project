9509e05a8db8321f21a9c8c12c24928c89146c28
Siwon <bearsiwon@naver.com>
2025-07-29 10:47:06 +0900

    백엔드 포트 엔티티

--- proj/src/main/java/net/dima/proj/entity/PortNameEntity.java
+++ proj/src/main/java/net/dima/proj/entity/PortNameEntity.java
@@ -1,5 +1,15 @@
 package net.dima.proj.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name="port_name")
+@Data
+@Builder
+
 public class PortNameEntity {
     
 }
