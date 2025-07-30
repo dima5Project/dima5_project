dc3c8d2d0b5d6965adaaef9594d0fe404572af78
Siwon <bearsiwon@naver.com>
2025-07-30 12:16:15 +0900

    0730 12:15 메인컨트롤러 생성

---
+++ src/main/java/net/dima/dima5_project/controller/MainController.java
@@ -0,0 +1,17 @@
+package net.dima.dima5_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    
+    /**
+     * 첫 화면 요청
+     */
+    @GetMapping("/")
+    public String getMethodName() {
+        return "index";
+    }
+    
+}
