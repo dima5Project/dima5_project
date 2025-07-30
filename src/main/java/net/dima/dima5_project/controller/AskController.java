fda4b8def0f77f234abafa72cd82ff127bc7a27c
Hyejin Kim <beckey7124@gmail,com>
2025-07-30 17:14:02 +0900

    2차 중간점검!!

--- src/main/java/net/dima/dima5_project/controller/AskController.java
+++ src/main/java/net/dima/dima5_project/controller/AskController.java
@@ -1,6 +1,5 @@
 package net.dima.dima5_project.controller;
 
-import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -11,12 +10,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dima.dima5_project.dto.AskBoardDTO;
 import net.dima.dima5_project.service.AskService;
+import net.dima.dima5_project.util.PageNavigator;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping
+@RequestMapping("/ask")
 @Slf4j
 @RequiredArgsConstructor
 public class AskController {
@@ -29,8 +31,8 @@ public class AskController {
     @Value("${spring.servlet.multipart.location}")
     String uploadPath;
 
-    @GetMapping("/askList")
-    public void askList(
+    @GetMapping("/ask")
+    public String ask(
             @PageableDefault(page = 1) Pageable pageable,
             @RequestParam(name = "searchItem", defaultValue = "askTitle") String searchItem,
             @RequestParam(name = "searchWord", defaultValue = "") String searchWord,
@@ -40,5 +42,28 @@ public class AskController {
 
         // DB에서 데이터 가져옴
         Page<AskBoardDTO> list = askService.selectAll(pageable, searchItem, searchWord);
+
+        int totalPages = list.getTotalPages(); // 전체 페이지 수
+        int page = pageable.getPageNumber(); // 사용자가 요청한 페이지
+
+        PageNavigator navi = new PageNavigator(pageLimit, page, totalPages);
+
+        model.addAttribute("list", list);
+        model.addAttribute("searchItem", searchItem);
+        model.addAttribute("searchWord", searchWord);
+        model.addAttribute("navi", navi);
+
+        return "ask";
     }
+
+    /**
+     * 글쓰기 화면 요청
+     * 
+     * @return
+     */
+    @GetMapping("/write")
+    public String write() {
+        return "ask/write";
+    }
+
 }
