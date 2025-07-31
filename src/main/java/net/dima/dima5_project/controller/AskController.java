c6c9fcd360f619d86184acd5f59dc9ce6793b2e3
Hyejin Kim <beckey7124@gmail,com>
2025-07-31 14:28:37 +0900

    ask 수정

--- src/main/java/net/dima/dima5_project/controller/AskController.java
+++ src/main/java/net/dima/dima5_project/controller/AskController.java
@@ -1,5 +1,9 @@
 package net.dima.dima5_project.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -15,13 +19,21 @@ import net.dima.dima5_project.util.PageNavigator;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/ask")
 @Slf4j
 @RequiredArgsConstructor
 public class AskController {
+
     private final AskService askService;
 
     // 한페이지에 보여줄 글 갯수
@@ -67,6 +79,61 @@ public class AskController {
     @GetMapping("/write")
     public String write() {
         return "ask/write";
+    }
+
+    /**
+     * 글 자세히 보기
+     * 
+     * @param askboardDTO
+     * @return
+     */
+    @PostMapping("/askWrite")
+    public String askWrite(@ModelAttribute AskBoardDTO askboardDTO) {
+        askService.insertAskBoard(askboardDTO);
+        return "redirect:/ask/askList";
+    }
+
+    /**
+     * 답변 토글 열리기 + 조회
+     */
+    public String askDetail(@RequestParam(name = "askSeq") Long askSeq,
+            @RequestParam(name = "searchItem", defaultValue = "askTitle") String searchItem,
+            @RequestParam(name = "searchWord", defaultValue = "") String searchWord, Model model) {
+        AskBoardDTO askBoardDTO = askService.checkOne(askSeq);
+        model.addAttribute("ask", askBoardDTO);
+        model.addAttribute("searchItem", searchItem);
+        model.addAttribute("searchWord", searchWord);
+        return "ask/askDetailAjax :: detailFragment";
+    }
+
+    /**
+     * 문의 글 삭제
+     */
+    @GetMapping("/askDelete")
+    public String askDelete(@RequestParam(name = "askSeq") Long askSeq,
+            @RequestParam(name = "searchItem", defaultValue = "askTitle") String searchItem,
+            @RequestParam(name = "searchWord", defaultValue = "") String searchWord, RedirectAttributes rttr) {
+        askService.deleteOne(askSeq);
+        rttr.addAttribute("searchItem", searchItem);
+        rttr.addAttribute("searchWord", searchWord);
+
+        return "redirect:/ask/askList";
+    }
+
+    /**
+     * 첨부파일 다운로드
+     */
+    @GetMapping("/download")
+    public String download(@RequestParam(name = "askSeq") Long askSeq, HttpServletResponse response) {
+        AskBoardDTO askBoardDTO = askService.checkOne(askSeq);
+        log.info("첨부파일명: {}", askBoardDTO.getSavedFilename());
+        String originalFilename = askBoardDTO.getOriginalFilename();
+        String savedFilename = askBoardDTO.getSavedFilename();
+        try{
+            String tempName = URLEncoder.encode(
+                originalFilename, StandardCharsets.UTF_8.toString());
+                response.setHeader("Content-Disposition", "attachment;filename=" + tempName);
+        } catch (UnsupportedEncodingException e)
     }
 
 }
--- src/main/java/net/dima/dima5_project/entity/AskBoardEntity.java
+++ src/main/java/net/dima/dima5_project/entity/AskBoardEntity.java
@@ -81,4 +81,5 @@ public class AskBoardEntity {
                 .reply(askBoardDTO.getReply())
                 .build();
     }
+
 }
