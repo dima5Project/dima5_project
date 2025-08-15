ea0b9bdafd58ca11db223d58014926978548e906
Geunhwa Park <sujanpk00@naver.com>
2025-08-15 13:45:40 +0900

    08.15 네이게이션바 수정

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     let nav = document.querySelector(".navbar");
     let currentPath = window.location.pathname; // 페이지 로드 시 URL 확인
+    let navItems = document.querySelectorAll('.navbar_menu .nav_item');
+    let navItemsSubmenu = document.querySelectorAll('.navbar_menu .nav_item.has_submenu');
 
     // 언어 드롭다운 (click으로 제어)
     let langToggle = document.getElementById("langToggle");
@@ -37,4 +39,38 @@ document.addEventListener("DOMContentLoaded", () => {
         nav.classList.add("expended");
         nav.dataset.fixedExpended = "true"; // 스크롤 이벤트에서 제거되지 않도록 플래그 설정
     }
+
+
+    // 해당 페이지 이동 시 색 표시
+    navItems.forEach(item => {
+        let activePathPrefix = item.getAttribute('data-active-path-prefix');
+
+        if (activePathPrefix && currentPath.startsWith(activePathPrefix)) {
+            item.classList.add('active');
+            // 해당 .nav_item 내의 .nav_link에 active 클래스 추가
+            let link = item.querySelector('.nav_link');
+            if (link) {
+                link.classList.add('active');
+            }
+        }
+    });
+
+    // 호버 시 서브메뉴 존재하면 너비 계산
+    navItemsSubmenu.forEach(item => {
+        // 마우스가 메뉴에 올라갔을 때 이벤트
+        item.addEventListener('mouseover', function () {
+            const submenu = this.querySelector('.submenu');
+            // 서브메뉴가 존재하면 너비를 계산하여 CSS 변수에 저장
+            if (submenu) {
+                const submenuWidth = submenu.offsetWidth;
+                this.style.setProperty('--underline-width', `${submenuWidth}px`);
+            }
+        });
+
+        // 마우스가 메뉴에서 벗어났을 때 이벤트
+        item.addEventListener('mouseout', function () {
+            // CSS 변수 초기화
+            this.style.setProperty('--underline-width', '0');
+        });
+    });
 });
