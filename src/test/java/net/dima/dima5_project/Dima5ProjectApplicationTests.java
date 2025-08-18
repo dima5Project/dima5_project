467f1957b841ad0012bfffeb89ca0d8e13edb9a5
Siwon <bearsiwon@naver.com>
2025-08-18 11:59:02 +0900

    0818 11:58 FastAPI ETA 추가 완료

--- src/test/java/net/dima/dima5_project/Dima5ProjectApplicationTests.java
+++ src/test/java/net/dima/dima5_project/Dima5ProjectApplicationTests.java
@@ -18,34 +18,34 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @ActiveProfiles("test") // 테스트 DB 설정 쓰는 경우 유지, 없으면 지워도 됨
 class Dima5ProjectApplicationTests {
 
-    @Autowired
-    private VesselMasterRepository vesselMasterRepository;
+    // @Autowired
+    // private VesselMasterRepository vesselMasterRepository;
 
 	@Autowired
     private VesselService vesselService;
 
-    @Test
-    void probeWithTypeAndQuery() {
-        // 입력값 정리(공백 제거)
-        String mmsi = "352978125".trim();
-		System.out.println(mmsi);
-        Optional<String> vslId = vesselMasterRepository.findVslIdByVslMmsi(mmsi);
+    // @Test
+    // void probeWithTypeAndQuery() {
+    //     // 입력값 정리(공백 제거)
+    //     String mmsi = "352978125".trim();
+	// 	System.out.println(mmsi);
+    //     Optional<String> vslId = vesselMasterRepository.findVslIdByVslMmsi(mmsi);
 
-        vslId.ifPresentOrElse(
-            id -> System.out.println("vsl_id = " + id),
-            () -> System.out.println("NOT_FOUND: mmsi=" + mmsi)
-        );
+    //     vslId.ifPresentOrElse(
+    //         id -> System.out.println("vsl_id = " + id),
+    //         () -> System.out.println("NOT_FOUND: mmsi=" + mmsi)
+    //     );
 
-        // 실제 테스트로 쓰려면 존재를 보장하고 싶을 때만 아래 assertion 유지
-        assertTrue(vslId.isPresent(), "vsl_id not found for mmsi=" + mmsi);
-    }
+    //     // 실제 테스트로 쓰려면 존재를 보장하고 싶을 때만 아래 assertion 유지
+    //     assertTrue(vslId.isPresent(), "vsl_id not found for mmsi=" + mmsi);
+    // }
 
     // (USE HERE !!!)
 	@Test
 	void predictByImoOrMmsi() throws Exception {
     
 		// 여기서 값 imo 또는 mmsi 지정
-    String imo  = "9187485";
+    String imo  = "9515606";
     String mmsi = "373126000";
 
     Map<String, Object> result = vesselService.predictByImoOrMmsi(imo, mmsi);
