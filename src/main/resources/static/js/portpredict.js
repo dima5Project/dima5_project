f1b64bbbadecbc5a85b9d3392419f0d80c7e9514
DESKTOP-PU1IG4U\Hyejin kim <beckey7124@naver.com>
2025-12-06 01:26:30 +0900

    js수정

--- src/main/resources/static/js/portpredict.js
+++ src/main/resources/static/js/portpredict.js
@@ -143,7 +143,7 @@ $(function () {
 
         // API 호출 로직
         try {
-            const response = await fetch(`http://127.0.0.1:8000/predict_map_by_vsl?vsl_id=${vslIdToFetch}`);
+            const response = await fetch(`http://dima5-fastapi.onrender.com/predict_map_by_vsl?vsl_id=${vslIdToFetch}`);
 
             // 응답 Content-Type 확인
             const contentType = response.headers.get("content-type");
